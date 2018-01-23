71c7f765cb48f492a6e2cb4cffbe889b03938fd4
Ryan Manchester <ryanm17@gmail.com>
2018-01-23 02:18:53 +0000

    automatically backed up by learn

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,18 @@
+module Memorable
+  def self.reset_all
+    self.all.clear
+  end
+
+  def self.count
+    @@artists.count
+  end
+
+  def self.reset_all
+    @@songs.clear
+  end
+
+  def self.count
+    self.all.count
+  end
+
+end
